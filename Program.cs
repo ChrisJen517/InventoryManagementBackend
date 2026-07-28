092f9bb27010dc0a298c0c27af8b22bfb9885636
Christian Jensen <chrisjen517@gmail.com>
2026-07-28 18:50:16 -0400

    Checking all connection strings

--- Program.cs
+++ Program.cs
@@ -29,15 +29,23 @@ builder.Services.AddCors(options =>
 string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 if (!builder.Environment.IsDevelopment())
 {
-    connectionString = builder.Configuration.GetConnectionString("MyDbConnection");
-    if (string.IsNullOrEmpty(connectionString))
+    var connectionStringsSection = builder.Configuration.GetSection("ConnectionStrings");
+    foreach (var connString in connectionStringsSection.GetChildren())
     {
-        connectionString = Environment.GetEnvironmentVariable("MYSQLCONNSTR_MyDbConnection"); ;
-        if (string.IsNullOrEmpty(connectionString))
-        {
-            throw new InvalidOperationException("Connection string 'AZURE_MYSQL_CONNECTIONSTRING' not found.");
-        }
+        // connectionString.Key is the name, connectionString.Value is the actual connection string
+        Console.WriteLine($"Key: {connString.Key}");
+        Console.WriteLine($"Value: {connString.Value}");
+        Console.WriteLine("-----------------------------------");
     }
+    // connectionString = builder.Configuration.GetConnectionString("MyDbConnection");
+    // if (string.IsNullOrEmpty(connectionString))
+    // {
+    //     connectionString = Environment.GetEnvironmentVariable("MYSQLCONNSTR_MyDbConnection"); ;
+    //     if (string.IsNullOrEmpty(connectionString))
+    //     {
+    //         throw new InvalidOperationException("Connection string 'AZURE_MYSQL_CONNECTIONSTRING' not found.");
+    //     }
+    // }
 }
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
